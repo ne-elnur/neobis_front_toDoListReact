6388d20c7f2b1763f9b924634d4f14dfe27403ad
ne-elnur <elnurkazy.urmat89@gmail.com>
2023-05-24 16:15:38 +0600

    init: app start

---
+++ todo-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ToDoList from "./components/ToDoList";
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-react/src/components/ToDoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ToDoList = () => {
+    return (
+        <div>
+            <h3>React ToDoList</h3>
+            <button>Add ToDoItem</button>
+        </div>
+    )
+}
+
+export default ToDoList;
