6a9372817f4af3a4bd71f1e159916ada53c9713a
ne-elnur <elnurkazy.urmat89@gmail.com>
2023-05-26 18:56:35 +0600

    feats: add Name

---
+++ todo-react/src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+function Header() {
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        const savedName = localStorage.getItem("name");
+        if (savedName) {
+            setName(savedName);
+        }
+    }, []);
+
+    const handleNameChange = (event) => {
+        const newName = event.target.value;
+        setName(newName);
+        localStorage.setItem("name", newName);
+    };
+
+    return (
+        <section className="greetings">
+            <h2 className="title">
+                What's up, <input type="text" value={name} placeholder="enter your name" onChange={handleNameChange}/>
+            </h2>
+        </section>
+    )
+}
+
+export default Header;
